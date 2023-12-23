b0a3f89b72796ad1c2c36fbcc0da33264c843bfb
tjdhg456 <hogili89@gist.ac.kr>
2023-12-23 16:00:57 +0900

    --

--- face_recognition/scripts/MS1MV2/run_main_fskd.sh
+++ face_recognition/scripts/MS1MV2/run_main_fskd.sh
@@ -1,5 +1,5 @@
 ## Teacher Training
-for MARGIN in CosFace ArcFace AdaFace
+for MARGIN in ArcFace AdaFace
 do
     SEED=5
 
@@ -18,15 +18,6 @@ do
     POOLING=E
     python train_teacher.py --seed $SEED --gpus 0 --data_dir /data/sung/dataset/face_dset/ --save_dir checkpoint/teacher-$DATASET/$BACKBONE-$POOLING-IR-$MARGIN/seed{$SEED} \
                             --down_size $RESOLUTION --mode ir --margin_type $MARGIN --pooling $POOLING --backbone $BACKBONE --dataset $DATASET --batch_size 512 --margin_float $MARGIN_F
-
-
-    # Naive Train
-    DATASET=ms1mv2
-    RESOLUTION=1
-    POOLING=E
-    INTERPOLATION=random
-    python train_teacher.py --seed $SEED --gpus 0 --data_dir /data/sung/dataset/face_dset/ --save_dir checkpoint/naive-$DATASET/$BACKBONE-$POOLING-IR-$MARGIN/resol$RESOLUTION-$INTERPOLATION/seed{$SEED} \
-                            --backbone $BACKBONE --down_size $RESOLUTION --pooling $POOLING --interpolation $INTERPOLATION --mode ir --margin_type $MARGIN --dataset $DATASET --batch_size 512 --margin_float 0.2
 done
 
 
