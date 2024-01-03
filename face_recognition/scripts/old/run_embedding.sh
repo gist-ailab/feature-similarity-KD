c032b3d8ebe0d28253340b38f0665c60ce477257
tjdhg456 <hogili89@gist.ac.kr>
2024-01-03 11:28:16 +0900

    --

--- face_recognition/scripts/old/run_embedding.sh
+++
@@ -1,2 +0,0 @@
-MARGIN=AdaFace
-python train_embed.py --mode ir --backbone iresnet50 --gpus 2 --data_dir /home/jovyan/SSDb/sung/dataset/face_dset --teacher_path checkpoint/teacher-casia/iresnet50-$MARGIN/last_net.ckpt
--- face_recognition/scripts/webface_main.sh
+++ face_recognition/scripts/webface_main.sh
@@ -1,11 +1,11 @@
-# CASIA
+# Teacher - Naive - F-SKD
 S=64.0
 DATASET=webface4m
 SEED=5
 POOLING=E                 
 DATA_DIR=/SSDb/sung/dataset/face_dset/
 
-for MARGIN in ArcFace
+for MARGIN in AdaFace
 do
     for MP in True
     do
@@ -17,7 +17,7 @@ do
             M_F=0.4
         fi
         RESOLUTION=0
-        CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nnodes=1 --nproc_per_node=2 --master_port=151 train_teacher_multi.py \
+        CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nnodes=1 --nproc_per_node=2 --master_port=530 train_teacher_multi.py \
                                 --seed $SEED --data_dir $DATA_DIR --save_dir checkpoint/teacher-$DATASET/iresnet50-$MARGIN-m{$M_F}-s{$S}-MP{$MP} \
                                 --down_size $RESOLUTION --mode ir --margin_type $MARGIN --pooling $POOLING --backbone iresnet50 --dataset $DATASET --batch_size 512 \
                                 --scale $S --margin_float $M_F --mixed_precision $MP
@@ -32,7 +32,7 @@ do
             M_F=0.4
         fi
         TEACHER=checkpoint/teacher-$DATASET/iresnet50-$MARGIN-m{$M_F}-s{$S}-MP{$MP}/last_net.ckpt
-        python train_embed.py --gpus 6 --dataset $DATASET --data_dir $DATA_DIR --mode ir --seed $SEED --teacher_path $TEACHER
+        python train_embed.py --gpus 2 --dataset $DATASET --data_dir $DATA_DIR --mode ir --seed $SEED --teacher_path $TEACHER
     
 
         # Student
@@ -49,7 +49,7 @@ do
         PARAM=20.0,4.0
         TEACHER=checkpoint/teacher-$DATASET/iresnet50-$MARGIN-m{$M_F}-s{$S}-MP{$MP}/last_net.ckpt
         SAVE_DIR=checkpoint/student-$DATASET/$BACKBONE-$MARGIN-m{0.2}-s{$S}-MP{$MP}/seed{$SEED}
-        CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nnodes=1 --nproc_per_node=2 --master_port=671 train_student_multi.py --seed $SEED --data_dir $DATA_DIR --down_size $RESOLUTION \
+        CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nnodes=1 --nproc_per_node=2 --master_port=531 train_student_multi.py --seed $SEED --data_dir $DATA_DIR --down_size $RESOLUTION \
                                                                     --backbone $BACKBONE --mode ir --margin_type $MARGIN --pooling $POOLING \
                                                                     --distill_type $METHOD --distill_param $PARAM --teacher_path $TEACHER \
                                                                     --save_dir $SAVE_DIR --batch_size 512 --dataset $DATASET --cross_margin 0.0 \
